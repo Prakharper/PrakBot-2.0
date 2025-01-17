f19c5fc422f145a6087baf0c115079aec88b082b
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-17 15:02:56 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -3,29 +3,13 @@ let staff = `🚩 *EQUIPO DE AYUDANTES*
 🤖 *Bot:* ${global.botname}
 🪐 *Versión:* ${global.vs}
 
-• 𝐃𝐚𝐫𝐤𝐂𝐨𝐫𝐞
+• *Matteus Sobrino* 
 🎩 *Rol:* Experiencia En Javascript y html
-🍭 *Número:* Wa.me/584120346669
+🍭 *Número:* Wa.me/573185444099
 
-• 𝐏𝐫𝐚𝐤 𝐇𝐚𝐫𝐩𝐞𝐫 
+• *Aamōn*  
 🎩 *Rol:* Experiencia En Javascript, Mantenimiento y Editor Profesional
-🍭 *Número:* Wa.me/584241836217
-
-• 𝐈𝐬𝐚𝐛𝐞𝐥 (isa)
-🎩 *Rol:* Experiencia En Diseños De Logos /Editora Profesional
-💛 *Número:* Wa.me/529831715910
-
-• 𝐒𝐞𝐧𝐧𝐚 
-🎩 *Rol:* Experiencia En Mantenimiento y Soporte Del Bot
-🍭 *Número:* Wa.me/573001357781
-
-• 𝐃𝐞𝐲𝐥𝐢𝐧 
-🎩 *Rol:* Experiencia En Redacción y Decoración De Textos
-💛 *Número:* Wa.me/50488198573
-
-• 𝐉𝐨𝐤𝐞𝐫 𝐲 𝐖𝐡𝐢𝐭𝐞 
-🎩 *Rol:* Bots Oficiales
-🍭 *Número:* Privado🫵🏻
+🍭 *Número:* Wa.me/529983694618
 `
 await conn.sendFile(m.chat, 'https://files.catbox.moe/sx42eg.jpg', 'brook.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
