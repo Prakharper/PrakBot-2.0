b12dc6d3714e0917413e70124a80b5721f6b1276
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-17 16:47:25 -0400

    Actualizar main-menu2.js.

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -23,7 +23,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
  await m.react(emojis)
 const document = doc[Math.floor(Math.random() * doc.length)];
- const str = `*☁ M E N U  - A U D I O S ☁*
+ const str = `*🚀 M E N U  - A U D I O S 🚀*
 
 *NO ES NECESARIO USAR PREFIJO EN AUDIOS*
 ∘ _Negro_
