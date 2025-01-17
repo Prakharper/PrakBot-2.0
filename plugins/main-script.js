6a9e3051f2c27e9b8650beeef5b10714ace8d099
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-17 16:43:57 -0400

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://api.github.com/repos/WillZek/CrowBot-ST')
+let res = await fetch('https://github.com/WillZek/PrakBot-2.0.git')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
