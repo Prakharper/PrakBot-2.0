8ec298fed5707232cf2371d3c6ff998afa30b381
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-30 19:56:37 -0400

    Actualizar main-menufun.js

--- plugins/main-menufun.js
+++ plugins/main-menufun.js
@@ -10,13 +10,13 @@ import fetch from 'node-fetch'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 const defaultMenu = {
-  before: `“🎳¡Hola! *%name*, a qui esta mi menu de juegos"
+  before: `“🚀¡Hola! *%name*, a qui esta mi menu de juegos"
 
 *─ׄ─ׄ─⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׅ──ׄ*
 %readmore
 `.trimStart(),
   header: '┏━━⪩「 *_`%category`_* 」⪨',
-  body: '┃『🎮』 %cmd\n',
+  body: '┃『🧿』 %cmd\n',
   footer: '┗━━━━━━━━━━━━━━━━⪩\n',
   after: ``,
 }
