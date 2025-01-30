11adbe8f06fb7d7011d6e1985738f8f6f26fc2f1
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-30 13:16:11 -0600

    Actualizar tools-luminai.js

---
+++ plugins/tools-luminai.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '❀ ingresa un texto para hablar con chatgpt', m)
+try {
+let api = await fetch(`https://api.davidcyriltech.my.id/ai/chatbot?query=${text}`)
+let json = await api.json()
+let JT = { contextInfo: { externalAdReply: { title: 'Chatgpt - Ai', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://chatgpt.com', sourceUrl: 'https://chatgpt.com', thumbnailUrl: 'https://i.pinimg.com/originals/9d/e4/97/9de497cbac67554d199b5945006f14d0.jpg', renderLargerThumbnail: false }}}
+
+  await conn.reply(m.chat, json.result, m, JT)
+
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['lumi']
+
+export default handler
