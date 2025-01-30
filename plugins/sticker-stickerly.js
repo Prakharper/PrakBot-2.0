da7026ffc89044f0973181eaee7406e6e17d9a08
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-30 12:49:23 -0600

    Actualizar sticker-stickerly.js

--- plugins/sticker-stickerly.js
+++ plugins/sticker-stickerly.js
@@ -2,36 +2,57 @@
 - https://github.com/WillZek 
 */
 
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, text }) => {
-    const stickerUrl = text.trim();
-    const apiUrl = `https://delirius-apiofc.vercel.app/download/stickerly?url=${encodeURIComponent(stickerUrl)}`;
-
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    let stiker = false;
     try {
-        const response = await fetch(apiUrl);
-        const json = await response.json();
-
-m.reply(`${json.error}`);
-        if (Array.isArray(json.stickers)) {
-            for (let sticker of json.stickers) {
-                if (sticker.url) {
-                    await conn.sendFile(m.chat, sticker.url, 'sticker.webp', m);
+        if (args[0]) {
+            if (isUrl(args[0])) {
+                const url = `https://delirius-apiofc.vercel.app/download/stickerly?url=${args[0]}`;
+                const res = await fetch(url);
+                const json = await res.json();
+                if (json.sticker) {
+                    stiker = json.sticker;
+                } else {
+                    return m.reply(`💫 No se encontraron stickers en esta URL.`);
                 }
+            } else {
+                return m.reply(`💫 El URL es incorrecto`);
             }
         } else {
-            m.reply(`🍭 No se encontraron stickers en la URL proporcionada.`);
+            return m.reply(`💫 Por favor, proporciona una URL de sticker.`);
         }
-
-    } catch (error) {
-        console.error(error.message);
-        m.reply(`🎩 Ocurrió un error: ${error.message}`);
+    } catch (e) {
+        console.error(e);
+        if (!stiker) stiker = e;
+    } finally {
+        if (stiker) {
+            conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, {
+                contextInfo: {
+                    'forwardingScore': 200,
+                    'isForwarded': false,
+                    externalAdReply: {
+                        showAdAttribution: false,
+                        title: textbot,
+                        body: dev,
+                        mediaType: 2,
+                        sourceUrl: args[0],
+                        thumbnail: icons
+                    }
+                }
+            }, { quoted: m });
+        } else {
+            return conn.reply(m.chat, `💫 *_La conversión ha fallado, intenta de nuevo._*`, m);
+        }
     }
 }
 
 handler.help = ['stickerly <url>'];
 handler.tags = ['sticker'];
-handler.command = ['stickerly', 'stlydl'];
-handler.estrellas = 4;
+handler.command = ['stickerly', 'slydl'];
+handler.estrellas = 3;
 
 export default handler;
+
+const isUrl = (text) => {
+    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'));
+}
