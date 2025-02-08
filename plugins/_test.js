31a2d7354e10526b443bc4a6013c7c6a7b082ae3
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-08 13:05:09 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let handler = async (m, { conn, text, groupMetadata, participants }) => {
 
-let user = a => '@' + a.split('@')[0];
+let user = a => '@+' + a.split('@')[0];
 let ps = groupMetadata.participants.map(v => v.id);
 let a = ps.getRandom();
 
