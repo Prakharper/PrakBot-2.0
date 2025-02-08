c0ffe6d4548e5a415a869a070e7ab394a28bd08f
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-08 09:28:39 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,7 +1,14 @@
+/* Twitterdl By WillZek 
+- Free Codes Titan
+- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
+*/
+
+// [🌠] Twitter Downloader
+
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('⬇️ Ingresa Un Link De Twitter Para Poder Mandar Su Video o Imagen');
+if (!args[0]) return m.reply('⬇️ Ingresa Un Link De Twitter');
 
 try {
 let api = `https://delirius-apiofc.vercel.app/download/twitterdl?url=${args[0]}`;
@@ -19,14 +26,14 @@ let arch = media[0];
 
 if (json.type === 'video') {
 let videoUrl = arch.url;
-let txt = `> *¡Descargado con éxito!*`;
+let txt = `> *¡Descargado con Exito!*`;
 
 await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: txt }, { quoted: fkontak });
 m.react('✅');
 
 } else if (json?.type === 'image') {
 let imageUrl = arch.url;
-await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: '¡Imagen descargada con éxito!' }, { quoted: fkontak });
+await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: '¡Descargado Con Exito!' }, { quoted: fkontak });
 m.react('✅');
 } else {
 return m.reply('✖️ El enlace no es ni una imagen ni un video.');
@@ -40,7 +47,7 @@ m.react('✖️');
 
 handler.help = ['xdl'];
 handler.tag = ['descargas'];
-handler.command = ['xdl', 'twitterdl']
+handler.command = ['xdl', 'twitterdl', 'test']
 handler.estrellas = 5;
 
 export default handler;
