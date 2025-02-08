3468742e8a838bdc92f66674baf53ccabb552f12
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-08 16:40:14 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -21,7 +21,7 @@ let txt = `*Nombre:* ${json.trackName}\n*Artista:* ${json.artistName}\n*Url:* ${
 
 let img = json.images.jpg.image_url;
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+conn.sendMessage(m.chat, { text: txt }, { quoted: fkontak });
 
 } catch (error) {
 console.log(error)
