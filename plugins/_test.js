0af83466d0ecd11172395587b38382592f8f3fde
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-08 13:04:00 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -2,26 +2,26 @@ import axios from 'axios';
 
 let handler = async (m, { conn, text, groupMetadata, participants }) => {
 
-let user = a => '@' + a.split('@')[0]
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
+let user = a => '@' + a.split('@')[0];
+let ps = groupMetadata.participants.map(v => v.id);
+let a = ps.getRandom();
 
-    const groupAdmins = participants.filter(p => p.admin);
-    const botId = conn.user.jid;
-    const groupOwner = groupAdmins.find(p => p.isAdmin)?.id;
-    const groupNoAdmins = participants.filter(p => p.id !== botId && p.id !== groupOwner && !p.admin);
+const groupAdmins = participants.filter(p => p.admin);
+const botId = conn.user.jid;
+const groupOwner = groupAdmins.find(p => p.isAdmin)?.id;
+const groupNoAdmins = participants.filter(p => p.id !== botId && p.id !== groupOwner && !p.admin);
 
-    if (groupNoAdmins.length === 0) throw '*⚠️ No hay usuarios para eliminar.*'; 
+if (groupNoAdmins.length === 0) throw '*⚠️ No hay usuarios para eliminar.*'; 
 
-    const randomUser  = groupNoAdmins[Math.floor(Math.random() * groupNoAdmins.length)];
+const randomUser  = groupNoAdmins[Math.floor(Math.random() * groupNoAdmins.length)];
 
-    const stickerUrl = 'https://files.catbox.moe/agx2sc.webp'; 
-    m.react('💫');
-    await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', m, null);
+const stickerUrl = 'https://files.catbox.moe/agx2sc.webp'; 
+m.react('💫');
+await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', m, null);
 
-    await conn.groupParticipantsUpdate(m.chat, [randomUser .id], 'remove');
-    conn.reply(m.chat, `*⚔️ Eliminación Exitosa de ${user(a)}`, m, rcanal);
-    m.react('✅');
+await conn.groupParticipantsUpdate(m.chat, [randomUser .id], 'remove');
+conn.reply(m.chat, `*⚔️ Eliminación Exitosa de ${user(randomUser .id)}*`, m, null);
+m.react('✅');
 }
 
 handler.help = ['kickrandom']
