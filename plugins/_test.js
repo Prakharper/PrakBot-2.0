d6408e5563d1b0907dddbd5054238cc02e9b33f7
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 16:12:48 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,17 +1,17 @@
 let handler = async (m, { args, conn }) => {
+
 let chat = global.db.data.chats[m.chat];
 if (!(m.chat in global.db.data.chats)) {
 return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m, fake);
 }
 
-if (args[0] == 'off') {
-
+if (args[0] == 'on') {
 chat.isBanned = true;
-conn.reply(m.chat, `✅ *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal);
-
-} else if (args[0] == 'on') {
-chat.isBanned = false;
 conn.reply(m.chat, `🍭 *CrowBot-ST ya fue desbaneado en este chat*`, m, rcanal);
+
+} else if (args[0] == 'off') {
+chat.isBanned = false;
+conn.reply(m.chat, `✅ *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal);
   }
 }
 
