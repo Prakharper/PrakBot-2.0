58cface87dfb8396894922e451081e08081d0210
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-10 19:33:50 -0600

    Actualizar plugins

---
+++ plugins/ff-mapa_aleatorio.js
@@ -0,0 +1,14 @@
+let handler = async(m, { conn }) => {
+
+let map = '*Aquí Tienes 🍨*';
+let mimg = [
+"https://files.catbox.moe/l3rzl1.jpg",
+"https://files.catbox.moe/mthxrp.jpg"
+].getRandom()
+
+conn.sendMessage(m.chat, { image: { url: mimg }, caption: map }, { quoted: fkontak });
+}
+
+handler.command = ['mapa'];
+
+export default handler 
