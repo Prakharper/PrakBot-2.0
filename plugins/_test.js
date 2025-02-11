114d98eb83147488cdef5829e608ed6275926f81
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 16:10:14 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,11 +1,11 @@
 let handler = async (m, { args, conn }) => {
-
 let chat = global.db.data.chats[m.chat];
 if (!(m.chat in global.db.data.chats)) {
 return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m, fake);
 }
 
 if (args[0] == 'off') {
+
 chat.isBanned = true;
 conn.reply(m.chat, `✅ *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal);
 
