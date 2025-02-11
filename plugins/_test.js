670978eaa7199cbe9faf45874c885991499b4fae
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 16:18:14 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,4 +1,34 @@
-let handler = async (m, { args, conn }) => {
+let handler = async (m, { conn, args }) => {
+let chat = global.db.data.chats[m.chat];
+
+if (!(m.chat in global.db.data.chats)) {
+return conn.reply(m.chat, '*El Bot No Está Registrado En Este Chat*', m);
+}
+
+if (chat.isBanned) {
+return conn.reply(m.chat, `${botname} Apagado`, m);
+}
+
+if (args[0] == 'on') {
+if (!chat.isBanned) {
+return conn.reply(m.chat, `*${botname} Ya Estaba Encendido*`, m);
+}
+
+chat.isBanned = false;
+await conn.reply(m.chat, `「✿」${botname} *Encendido*`, m);
+} else if (args[0] == 'off') {
+chat.isBanned = true;
+await conn.reply(m.chat, `「✿」${botname} *Apagado*`, m);
+} else {
+conn.reply(m.chat, `「✿」Usa Una Opción Valida`, m);
+    }
+}
+
+handler.command = ['test'];
+
+export default handler;
+
+/* let handler = async (m, { args, conn }) => {
 
 let chat = global.db.data.chats[m.chat];
 if (!(m.chat in global.db.data.chats)) {
@@ -18,3 +48,4 @@ conn.reply(m.chat, `✅ *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal);
 handler.command = ['test'];
 
 export default handler;
+*/
