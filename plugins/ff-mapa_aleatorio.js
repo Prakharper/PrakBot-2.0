45451bf31fd16a430d1f60e8b1c59970bd4a50e0
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-10 19:38:16 -0600

    Actualizar ff-mapa_aleatorio.js

--- plugins/ff-mapa_aleatorio.js
+++ plugins/ff-mapa_aleatorio.js
@@ -4,6 +4,7 @@ let map = '*Aquí Tienes 🍨*';
 let mimg = [ 
 'https://files.catbox.moe/l3rzl1.jpg',
 'https://files.catbox.moe/mthxrp.jpg',
+'https://files.catbox.moe/nejjwf.jpg',
 ].getRandom()
 
 conn.sendMessage(m.chat, { image: { url: mimg }, caption: map }, { quoted: fkontak });
