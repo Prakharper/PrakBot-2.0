82f0d0ecfeabb0a3bf375b01e9879749aca6f2f0
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-11 15:40:23 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -14,6 +14,7 @@ if (!chat.isBanned) return conn.reply(m.chat, '[🌠] *El bot no está baneado e
 chat.isBanned = false
 
 await conn.reply(m.chat, `${emojis} *CrowBot-ST ya fué desbaneado en este chat*`, m, fake)
+  }
 }
 
 handler.command = ['test']
