f5d88525a5488e743b4aa1a3868548c40a79f7ae
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:27:59 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -4,7 +4,7 @@ let pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'ht
 
 let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
 
-conn.sendMessage(m.chat, { image: { url: ppp, caption: txt }, { quoted: fkontak }});
+conn.sendMessage(m.chat, { image: { url: ppp, caption: 'Prueba' }, { quoted: fkontak }});
 }
 
 handler.command = ['wtest'];
