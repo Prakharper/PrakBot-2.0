91732d09e9974dff7976f7582a678975df408da2
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:26:11 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,4 +1,4 @@
-let handler = async(m, { conn }) => {
+let handler = async(m, { conn, command }) => {
 
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'https://files.catbox.moe/kd7vs5.jpg';
 
