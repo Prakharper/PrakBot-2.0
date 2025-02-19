1d01ce20d7bb21ceb6e3b7791583ff09e33d7821
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:32:54 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,10 +1,15 @@
+import fetch from 'node-fetch';
+
 let handler = async (m, { conn, usedPrefix, command }) => {
 
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'https://files.catbox.moe/kd7vs5.jpg';
 
 let p = 'https://files.catbox.moe/i8fk7k.jpg';
 
-let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
+let ppp = await fetch(`https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`).json
+
+/* let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
+*/
 
 conn.sendMessage(m.chat, { image: { url: ppp }, caption: 'Prueba' }, { quoted: fkontak });
 }
