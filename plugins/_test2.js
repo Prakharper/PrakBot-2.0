82fef28bea6c5c14942246a9bf79490ac13f7999
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:38:15 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -8,10 +8,9 @@ let p = 'https://files.catbox.moe/i8fk7k.jpg';
 
 let ppp = await fetch(`https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`);
 
-/* let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
-*/
+let buffer = await ppp.buffer();
 
-conn.sendMessage(m.chat, { image: { url: ppp }, caption: 'Prueba' }, { quoted: fkontak });
+conn.sendMessage(m.chat, { image: { url: buffer }, caption: 'Prueba' }, { quoted: fkontak });
 }
 
 handler.command = ['wtest'];
