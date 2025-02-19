959ab8cf2fbc556f20334a079e3faaeb56dc1294
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:25:10 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn }) => {
 
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'https://files.catbox.moe/kd7vs5.jpg';
 
-let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=${pp}&bg=${pp}&apikey=5f6682c9`;
+let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
 
 conn.sendMessage(m.chat, { image: { url: ppp }, caption: 'Prueba' }, { quoted: fkontak });
 }
