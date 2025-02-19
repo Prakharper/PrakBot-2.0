7fffbfd7cd0a21fabffee7bfcacd39fb1d6d3529
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-19 15:30:59 -0600

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -6,7 +6,7 @@ let p = 'https://files.catbox.moe/i8fk7k.jpg';
 
 let ppp = `https://api.lyrax.net/api/maker/welcome1?name=WillZek&gpname=CrowBot&member=3&pp=https://files.catbox.moe/i8fk7k.jpg&bg=https://files.catbox.moe/i8fk7k.jpg&apikey=5f6682c9`;
 
-conn.sendMessage(m.chat, { image: { url: p }, caption: 'Prueba' }, { quoted: fkontak });
+conn.sendMessage(m.chat, { image: { url: ppp }, caption: 'Prueba' }, { quoted: fkontak });
 }
 
 handler.command = ['wtest'];
