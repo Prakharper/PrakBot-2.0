c3f004e7a17a72d60c9d34fc50a3fa69f9fabfb2
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-23 15:20:52 -0400

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, usedPrefix, command, text }) => {
 
 if (!text) return m.reply(m.chat, `🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}crow edits`, m, rcanal);
 
-let api = await(await fetch(`https://dark-core-api.vercel.app/api/search/youtube?key=api&text=${text}`)).join();
+let api = await (await fetch(`https://dark-core-api.vercel.app/api/search/youtube?key=api&text=${text}`)).json();
 
 let ST = `✨ *Título:* ${api.results.title}\n📎 *Link:* ${api.results.url}\n🍭 *Canal:* ${api.results.channel}\n📆*Publicado:* ${api.results.published}`;
 
