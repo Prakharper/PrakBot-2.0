d6dacc20fbcc99f6ea2b4852e78f9fe39d48b418
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-24 17:58:57 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,5 +1,45 @@
-// *[ ❀ YTMP4 ]*
 import fetch from 'node-fetch';
+
+let handler = async(m, { conn, args, text }) => {
+
+if (!text) return m.reply('🍭 Ingrese Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh');
+
+m.react(rwait);
+
+let video;
+try {
+      video = await (await fetch(`https://api.alyachan.dev/api/ytv?url=${text}&apikey=Gata-Dios`)).json();
+} catch (error) {
+try {
+      video = await (await fetch(`https://api.fgmods.xyz/api/downloader/ytmp4?url=${text}&quality=480p&apikey=be9NqGwC`)).json();
+} catch (error) {
+try {
+      video = await (await fetch(`https://good-camel-seemingly.ngrok-free.app/download/mp4?url=${text}`)).json();
+} catch (error) {
+      video = await (await fetch(`https://dark-core-api.vercel.app/api/download/ytmp4?key=api&url=${text}`)).json();
+      }
+    }
+ }
+
+let link = video?.data?.url || video?.download_url || video?.result?.dl_url || video?.downloads?.link
+
+if (!link) return m.reply('No se pudo obtener el video.');
+
+await conn.sendMessage(m.chat, {
+      video: { url: link },
+      mimetype: "video/mp4",
+      caption: `${dev}`,
+    }, { quoted: m });
+    m.react(done);
+}
+
+handler.command = ['ytv', 'ytmp4', 'ymp4']
+
+export default handler;
+
+
+// *[ ❀ YTMP4 ]*
+/* import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) return conn.reply(m.chat, `💛 Ingresa un link de YouTube válido\n> Ejemplo https://youtu.be/P4LfHsUnNL8?si=ahDKJ5h0cW-EB9C9`, m, rcanal);
@@ -27,3 +67,4 @@ handler.estrellas = 4;
 handler.register = true;
 
 export default handler;
+*/
