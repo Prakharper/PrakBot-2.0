828496b1b8aedc5ee04b0abbc6e57275e51c82bf
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-24 22:58:09 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -6,41 +6,43 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, args, text }) => {
 
-if (!text) return m.reply('💖 Ingrese Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh');
+if (!text) return m.reply('🍭 Ingrese Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh');
 
 m.react(rwait);
-
-let video;
 
 try {
 let camel = await (await fetch(`https://good-camel-seemingly.ngrok-free.app/download/mp4?url=${text}`)).json();
 
-if (camel && camel.data) {
-            video = camel;
-} else {
-let dark = await (await fetch(`https://dark-core-api.vercel.app/api/download/ytmp4?key=api&url=${text}`)).json();
-if (dark && dark.downloads && dark.downloads.length > 0) {
-                video = dark;
-            }
-        }
-} catch (error) {
-return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
-    }
+let dark = await (await fetch(`https://dark-core-api.vercel.app/api/download/ytmp4?key=api&url=${text}`))[0].json();
 
-let link = video?.downloads?.find(d => d.quality === "360p")?.link ||
-               video?.downloads?.find(d => d.quality === "1080p50")?.link ||
-               video?.downloads?.find(d => d.quality === "480p")?.link;
+let video = camel || dark
+
+/*      video = await (await fetch(`https://dark-core-api.vercel.app/api/download/ytmp4?key=api&url=${text}`)).json();
+} catch (error) {
+try {
+      video = await (await fetch(`https://api.fgmods.xyz/api/downloader/ytmp4?url=${text}&quality=480p&apikey=be9NqGwC`)).json();
+} catch (error) {
+try {
+      video = await (await fetch(`https://api.alyachan.dev/api/ytv?url=${text}&apikey=uXxd7d`)).json();
+} catch (error) {
+      video = await (await fetch(`https://good-camel-seemingly.ngrok-free.app/download/mp4?url=${text}`)).json();
+      }
+    }
+ }
+*/
+
+let link = video?.data?.url || video?.download_url || video?.result?.dl_url || video?.downloads?.link
 
 if (!link) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
 
 await conn.sendMessage(m.chat, {
-        video: { url: link },
-        mimetype: "video/mp4",
-        caption: `${dev}`,
+      video: { url: link },
+      mimetype: "video/mp4",
+      caption: `${dev}`,
     }, { quoted: m });
-m.react(done);
+    m.react(done);
 }
 
-handler.command = ['ytv', 'ytmp4', 'ymp4'];
+handler.command = ['ytv', 'ytmp4', 'ymp4']
 
 export default handler;
