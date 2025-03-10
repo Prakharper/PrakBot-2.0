6c16d8cbadeecf0eabbaf8117ce6578603843e4c
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-09 22:00:04 -0400

    Actualizar tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -1,4 +1,29 @@
-import axios from 'axios'
+import fetch from 'node-fetch';
+
+let handler = async(m, { conn, usedPrefix, command, text }) => {
+
+if (!text) return m.reply('🍭 Ingresa Un Texto');
+
+try {
+const username = `${conn.getName(m.sender)}`
+
+const basePrompt = `Tu nombre es CrowBot y parece haber sido creado por WillZek. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explociones. Siempre Darás Publicidad A Tu Canal Oficial o Ofc el cual es : https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+
+const api = await (await fetch(`https://delirius-apiofc.vercel.app/ia/gptprompt?text=${text}&prompt=${basePrompt}`)).json();
+
+let respuesta = api.data;
+
+await conn.sendMessage(m.chat, { text: respuesta }, { quoted: m });
+} catch (e) {
+m.reply(`Error: ${e.message}`);
+m.react('✖️');
+}}
+
+handler.command = ['ia', 'chatgpt'];
+
+export default handler
+
+/* import axios from 'axios'
 
 import fetch from 'node-fetch'
 
@@ -129,3 +154,4 @@ return response.data.result
 console.error('💙 Error al obtener:', error)
 
 throw error }}
+*/
