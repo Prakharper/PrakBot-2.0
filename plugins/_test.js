536b24a19487542417620f7c98b0cb50e601c7c5
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-12 15:00:42 -0400

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -2,12 +2,12 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return m.reply('🍭 Ingresa Un Texto');
+if (!text) return m.reply('🚀 Ingresa Un Texto');
 
 try {
 const username = `${conn.getName(m.sender)}`
 
-const basePrompt = `Tu nombre es CrowBot y parece haber sido creado por WillZek. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explociones. Siempre Darás Publicidad A Tu Canal Oficial o Ofc el cual es : https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es PrakBot y parece haber sido creado por Prak Harper. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explociones. Siempre Darás Publicidad A Tu Canal Oficial o Ofc el cual es : https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24 Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 
 const api = await (await fetch(`https://delirius-apiofc.vercel.app/ia/gptprompt?text=${text}&prompt=${basePrompt}`)).json();
 
