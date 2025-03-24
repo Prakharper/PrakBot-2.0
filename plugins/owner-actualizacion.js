2df3919a57b21300beb7a9a47848f861467985d9
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-24 01:12:14 -0400

    Actualizar owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -6,7 +6,7 @@ let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
 const owner = 'ianalejandrook15x';
-const repo = 'Crow';
+const repo = 'Prak';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: estilo});  
 try {
