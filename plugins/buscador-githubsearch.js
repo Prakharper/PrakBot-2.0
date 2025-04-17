ecb5c48981ea2b65f3289c6ed098d531f14d6d4c
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-17 15:33:21 -0600

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,6 +1,6 @@
 /* Github Search By WillZek 
-- Free Codes Titan 
-- https://github.com/WillZek 
+- Free Codes Titan  
+- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
 */
 
 // [🔎] 𝗚𝗶𝘁𝗵𝘂𝗯 𝗦𝗲𝗮𝗿𝗰𝗵
@@ -9,27 +9,30 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply(m.chat, '🍭 Ingresa Un Nombre De Repositorio o De Usuario De Github', m, rcanal);
+if (!text) return m.reply('《★》Ingresa Un Nombre De Repositorio o De Usuario De Github');
 
 try {
-let api = 'https://dark-core-api.vercel.app/api/search/github?key=api&q=${text}';
+let api = `https://dark-core-api.vercel.app/api/search/github?key=dk-vip&text=${text}`;
 
-let responde = await fetch(api);
+let response = await fetch(api);
 let json = await response.json();
+let result = json.results[0];
 
-let txt = `Nombre: ${json.name}\nDescripcion: ${json.description}\nCreado: ${json.createdAt}`;
+let txt = `*Nombre:* ${result.name}\n*Owner:* ${result.creator}\n*Estrellas:* ${result.stars}\n*Bifurcaciones:* ${result.forks}\n*Descripcion:* ${result.description}\n*Creado:* ${result.createdAt}\n*Link:* ${result.cloneUrl}`;
 
-let img = 'https://cloud.dorratz.com/files/669d45d70d27913f08db78953c11903b';
+let img = 'https://files.catbox.moe/9vlgt5.jpg';
 
-conn.sendMessage(m.chat, { image: { url: img, caption: txt }, { quoted: fkontak }});
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
 
 } catch (error) {
-console.log(error)
-m.reply(`*Error:* ${error.message}`);
-m.reply('✖️');
+console.error(error)
+m.reply(`Error: ${error.message}`);
+m.react('✖️');
  }
 };
 
+handler.tag = ['buscador'];
+handler.help = ['githubsearch'];
 handler.command = ['githubsearch', 'gbsearch'];
 
 export default handler;
